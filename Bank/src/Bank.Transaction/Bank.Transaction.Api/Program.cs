fec12e01805ba2bdbd8e2be4afed59462d767f8a
Angel Bustamante <bustamanteangel532@gmail.com>
2025-11-09 12:54:48 -0600

    feat(transaccion-service): implementar arquitectura, configuración de base de datos e integración inicial.
    - Se creó la arquitectura base del microservicio de Transacción
    - Se configuró la conexión y el contexto de base de datos
    - Se implementaron las entidades, repositorios y mapeos iniciales
    - Se realizó la integración del servicio con la base de datos
    - Se validó la persistencia y lectura de datos mediante pruebas locales

---
+++ Bank/src/Bank.Transaction/Bank.Transaction.Api/Program.cs
@@ -0,0 +1,28 @@
+using Bank.Transaction.Api.Application.Database;
+using Bank.Transaction.Api.Application.External.ServiceBusSender;
+using Bank.Transaction.Api.Application.Features.Process;
+using Bank.Transaction.Api.Application.Handlers;
+using Bank.Transaction.Api.External.ServiceBusReceive;
+using Bank.Transaction.Api.External.ServiceBusSender;
+using Bank.Transaction.Api.Persistence.Database;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DatabaseService>(options =>
+{
+    options.UseSqlServer(builder.Configuration["TRANSACTIONSQLDBCONSTR"]);
+});
+
+builder.Services.AddScoped<IDatabaseService, DatabaseService>();
+builder.Services.AddScoped<IProcessService, ProcessService>();
+builder.Services.AddSingleton<IServiceBusSenderService, ServiceBusSenderService>();
+
+//trabajando en background (escuchando siempre)
+builder.Services.AddHostedService<ServiceBusReceiveService>();
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(typeof(ProcessHandler).Assembly));
+
+var app = builder.Build();
+
+app.Run();
+
